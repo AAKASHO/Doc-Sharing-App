10c88b0ec226370ef868a09519d5a8742927a020
aakasho <iamaakashkumar789@gmail.com>
2024-04-08 20:17:41 +0530

    changes

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv/config';
 
 const Connection = async () => {
-  const url ="mongodb://narutousumakitelegram:narutousumakitelegram@ac-xx95qx0-shard-00-00.kw3hdrc.mongodb.net:27017,ac-xx95qx0-shard-00-01.kw3hdrc.mongodb.net:27017,ac-xx95qx0-shard-00-02.kw3hdrc.mongodb.net:27017/?ssl=true&replicaSet=atlas-2xuuro-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster4";
+  const url ="mongodb://narutousumakitelegram:narutousumakitelegram@ac-qwzerbg-shard-00-00.a246cen.mongodb.net:27017,ac-qwzerbg-shard-00-01.a246cen.mongodb.net:27017,ac-qwzerbg-shard-00-02.a246cen.mongodb.net:27017/?ssl=true&replicaSet=atlas-8p3pyc-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster1";
   // console.log(url);
 
   try {
@@ -11,7 +11,7 @@ const Connection = async () => {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    
+
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error while connecting with the database ", error);
