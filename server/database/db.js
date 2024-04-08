ebb8169897019a3077f88f8c3b489c90de219290
aakasho <iamaakashkumar789@gmail.com>
2024-04-08 20:23:33 +0530

    mongo

--- server/database/db.js
+++ server/database/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv/config';
 
 const Connection = async () => {
-  const url ="mongodb://narutousumakitelegram:narutousumakitelegram@ac-qwzerbg-shard-00-00.a246cen.mongodb.net:27017,ac-qwzerbg-shard-00-01.a246cen.mongodb.net:27017,ac-qwzerbg-shard-00-02.a246cen.mongodb.net:27017/?ssl=true&replicaSet=atlas-8p3pyc-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster1";
+  const url ="mongodb://narutousumakitelegram:narutousumakitelegram@ac-xx95qx0-shard-00-00.kw3hdrc.mongodb.net:27017,ac-xx95qx0-shard-00-01.kw3hdrc.mongodb.net:27017,ac-xx95qx0-shard-00-02.kw3hdrc.mongodb.net:27017/?ssl=true&replicaSet=atlas-2xuuro-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster4";
   // console.log(url);
+  
 
   try {
     await mongoose.set('strictQuery', false);
